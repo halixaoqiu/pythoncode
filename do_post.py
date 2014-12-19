14cce393b9a120e71e60b9e7ed6e9b105ef14022
qiu.lin <qiu.lin@dianping.com>
2014-12-19 10:50:20 +0800

    code commit

--- do_post.py
+++ do_post.py
@@ -22,9 +22,9 @@ import hashlib
 
 # methodsendmobile18317211633num3out_order_id266486499s916691326t71seller_id1721305230160sub_outer_iid1721402260250timestamp2014-12-04 17:05:53total_fee180.00signc3fa9fc50c9bae1b0dafac11abbad33a
 
-data = {'method':'send','mobile':'18317211633','num':'3','out_order_id':'266486499s916691326t71','seller_id':'2673557717','sub_outer_iid':'1721305230160','timestamp':'2014-12-04 17:05:53','token':'null','total_fee':'180.00','sign':'D726E07E051618F8FF54919631FEDBDD'}
+data = {"refundTime":"2014-10-11 00:00:00","returnCode":0,"returnCode111":0,"serialId":"123456789","voucherNumberList":[{"voucherNumber":"123321"}]}
 data = urllib.urlencode(data)
-url = 'http://test1.allinpaymall.com:2303/MessageService/LMM.aspx'
+url = 'http://t.51ping.com/play/servlet/refundnotify'
 f = urllib2.urlopen(url, data)
 rt = f.read()
 print rt
